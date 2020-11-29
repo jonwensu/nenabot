a434723540815b79d624a4812e094e66979a6f7c
jon wensu <wens.sumande@gmail.com>
2020-11-29 21:19:06 +0800

    add permission check

--- commands/raffle.js
+++ commands/raffle.js
@@ -7,33 +7,46 @@ const {
 	getEmoji,
 	shuffle,
 	pick,
-	rateRoll,
 	createEmbedMessage,
 } = require("../util/formatUtil");
 const { keys: giftKeys, contents: giftContents } = require("../util/GiftKeys");
+const { HAS_RAFFLE_PERMISSION, OWNER_ID } = process.env;
 
 exports.run = async (client, message, args) => {
 	const action = (args[2] || "").toLowerCase();
-
+	let showAll = false;
 	switch (action) {
 		case "start":
-			await pickWinner(client, message);
+			const canStartRaffle =
+				HAS_RAFFLE_PERMISSION.split(",").includes(message.author.id) ||
+				OWNER_ID === message.author.id;
+			if (canStartRaffle) {
+				showAll = (args[3] || "").toLowerCase() === "f";
+				await pickWinner(client, message, showAll);
+			} else {
+				message.channel.send(
+					`${mentionAuthor(
+						message
+					)}, you do not have the permssion to start a raffle.`
+				);
+			}
 			break;
 		default:
-			await showParticipants(client, message);
+			showAll = action === "f";
+			await showParticipants(client, message, showAll);
 	}
 };
 
 exports.help =
 	"Just an example command. Usage: `${process.env.BOT_PREFIX}example`";
-exports.aliases = ["hl"];
+exports.aliases = ["rf"];
 
-async function pickWinner(client, message) {
+async function pickWinner(client, message, showAll) {
 	const spinner = getEmoji(client, "spin");
 	const nitro = giftContents(client)[giftKeys.nitro].icon;
 	const enclose = (m, icon = spinner) => `${icon} ${m} ${icon}`;
 	const m = await message.channel.send(enclose("Picking a winner"));
-	const participants = await getParticipants(client);
+	const participants = await getParticipants(client, message, showAll);
 
 	const pool = participants.reduce((prev, next) => {
 		return prev.concat(
@@ -73,17 +86,15 @@ async function assignNitro(message, client, target) {
 	const nitroService = new NitroService(client.database);
 
 	const pool = await nitroService.getRaffle();
-	console.log(target);
 
 	const entry = Object.keys(pool).find((k) => !pool[k].owner);
-	console.log("entry", entry);
 
 	if (entry) {
 		await nitroService.assignRaffle(entry, target.id);
 	}
 }
 
-async function getParticipants(client) {
+async function getParticipants(client, message, showAll = false) {
 	const raffleService = new RaffleService(client.database);
 	const userService = new UserService(client.database);
 	const inventoryService = new InventoryService(client.database);
@@ -93,6 +104,15 @@ async function getParticipants(client) {
 	const excluded = await raffleService.getExcluded();
 
 	const ticketId = giftContents(client)[giftKeys.potchicket].id;
+	const members = await message.channel.guild.members.fetch();
+
+	const extra = (u) => {
+		if (showAll) {
+			return true;
+		}
+		const a = members.some((m) => m.user.id === u.id);
+		return a;
+	};
 
 	return Object.keys(inventories)
 		.filter((k) => Object.keys(users).includes(k) && !excluded.includes(k))
@@ -105,7 +125,7 @@ async function getParticipants(client) {
 				entries,
 			};
 		})
-		.filter((a) => !!a);
+		.filter((a) => !!a && extra(a));
 }
 
 async function delay(ms) {
@@ -116,8 +136,8 @@ async function delay(ms) {
 	});
 }
 
-async function showParticipants(client, message) {
-	const participants = await getParticipants(client);
+async function showParticipants(client, message, showAll) {
+	const participants = await getParticipants(client, message, showAll);
 
 	if (participants && participants.length) {
 		const response = participants
